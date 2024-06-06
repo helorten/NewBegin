f33479eb40088901bc3808d75330ffc4fc302f24
helorten <poste4dima@gmail.com>
2024-06-06 11:02:57 +0200

    Add business model

---
+++ NewBegin.Data/Models/BusinessModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewBegin.Data.Models
+{
+    public class BusinessModel
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string OwnerID { get; set; }
+    }
+}
