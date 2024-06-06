470f04bdd539c815d24cafc98531acbcdb213cde
helorten <poste4dima@gmail.com>
2024-06-06 11:01:17 +0200

    Add business type enum

---
+++ NewBegin.Data/Enums/BusinessType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewBegin.Data.Enums
+{
+    internal enum BusinessType
+    {
+        Food,
+        Health,
+        Shop,
+        ManicureAndPedicure,
+        Massage,
+        Hairdressing,
+        Psychology,
+        Tattoo,
+        Sports,
+        AutoServices,
+        VideoServices,
+        Photography,
+        Animals, 
+        CarriageServices,
+        Design,
+        CleaningServices,
+        ComputerServices,
+        Furniture,
+        Music,
+        Education,
+        ClothingAndFootwear,
+        LegalServices,
+        RepairOfHouseholdAppliances,
+        RealtorServices,
+        SanitaryEngineering,
+        ConstructionAndRepair,
+        Dancing,
+        Tourism,
+        Floristics,
+        Electrics
+    }
+}
